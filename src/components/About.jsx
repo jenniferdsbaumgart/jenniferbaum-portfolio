08a77549dff00bdc08142c5a13fd98b43547fb1c
Jennifer Baumgart <jenniferdsbaumgart@gmail.com>
2025-05-16 15:41:13 -0300

    feat: updated CV and remove outdated files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,19 +13,19 @@ const About = () => {
                 <div className="max-w-[800px] rounded-xl bg-zinc-100 p-5 text-justify flex flex-col items-center dark:bg-zinc-800 transition-colors">
                     <span className="absolute -left-5 top-20 scale-[2.5] text-zinc-100 md:hidden dark:text-zinc-700 transition-colors">{arrowLeftIcon}</span>
                     <p className="text-lg font-light text-gray-700 first-letter;pl-3 lg:text-[16px] sm:text[14px] dark:text-white">{aboutText}</p>
-                    <a href="/nick-cv.pdf" download="" className="w-max px-7 py-3 flex items-center gap-x-2 mt-6 rounded-2xl border-gay-300 bg-violet-600 font-light text-white hover:bg-violet-400 transition-colors">
+                    <a href="/jenniferbaum-frontend-cv.pdf" download="" className="w-max px-7 py-3 flex items-center gap-x-2 mt-6 rounded-2xl border-gay-300 bg-violet-600 font-light text-white hover:bg-violet-400 transition-colors">
                         <span>Download CV</span>
                         <span className="text-xl">{downloadIcon}</span>
                     </a>
                 </div>
             </div>
-            <div className="mt-20 w-full flex flex-wrap items-center justify-between gap-x-7 gap-y-10">
+            {/* <div className="mt-20 w-full flex flex-wrap items-center justify-between gap-x-7 gap-y-10">
                 {aboutData.map((item, i) => (
                     <Achievements key={i} icon={item.icon} amount={item.amount} text={item.text}>
                         {item.icon} {item.text}
                     </Achievements>
                 ))}
-            </div>
+            </div> */}
         </div>
     );
 }
