d0864ed226aa69730b9ca351d085a4c0d99c5342
Jennifer Baumgart <jenniferdsbaumgart@gmail.com>
2025-05-16 15:39:55 -0300

    feat: update loading animation to use load-cat.gif and adjust transition duration

--- src/components/sub/Load.jsx
+++ src/components/sub/Load.jsx
@@ -12,10 +12,10 @@ const Load = () => {
     <motion.div
       initial={{ top: 0 }}
       animate={{ top: load ? "-100%" : 0 }}
-      transition={{ duration: 0.5 }}
+      transition={{ duration: 0.8 }}
       className="w-full h-full fixed left-0 top-0 flex items-center justify-center bg-gradient-to-t from-zinc-700 to-zinc-900 z-50"
     >
-      <img src="spinner.gif" />
+      <img src="load-cat.gif" />
     </motion.div>
   );
 };
